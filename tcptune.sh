c588737fef2ae5ff91808d2f977c04c62d7c92cf
Pedro Paulo Monte Pagani <56891617+PedroMPagani@users.noreply.github.com>
2026-03-22 08:58:27 -0300

    Update tcptune.sh

--- tcptune.sh
+++ tcptune.sh
@@ -8,14 +8,10 @@ sysctl -w net.ipv4.tcp_adv_win_scale=2
 sysctl -w net.core.netdev_max_backlog=8192
 sysctl -w net.core.netdev_budget=600
 sysctl -w net.core.netdev_budget_usecs=4000
-sysctl -w net.ipv4.tcp_mem="2097152 3145728 4194304"
-sysctl -w net.core.rmem_max=134217728
-sysctl -w net.core.wmem_max=134217728
-sysctl -w net.ipv4.tcp_rmem="4096 2097152 134217728"
-sysctl -w net.ipv4.tcp_wmem="4096 2097152 134217728"
-sysctl -w net.ipv4.tcp_mtu_probing=1
-sysctl -w net.core.busy_read=50
-sysctl -w net.core.busy_poll=50
+sysctl -w net.core.rmem_max=16777216
+sysctl -w net.core.wmem_max=16777216
+sysctl -w net.ipv4.tcp_rmem="4096 2097152 16777216"
+sysctl -w net.ipv4.tcp_wmem="4096 2097152 16777216"
 ip route show | while read -r route; do
     echo "$route" | grep -q "initcwnd" && continue
     ip route replace $route initcwnd $CWND initrwnd $CWND
